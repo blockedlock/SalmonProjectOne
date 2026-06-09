8abd2345d407c80d3cdc6390421909477619a97f
ChocoPie <gruskok3@gmail.com>
2026-06-09 16:20:03 +0300

    added method body to ProductRepository

--- src/main/java/com/myproject/salmon/repository/UserRepository.java
+++ src/main/java/com/myproject/salmon/repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.myproject.salmon.repository;
 
 import com.myproject.salmon.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    Optional<User> findByChatId(Long chatId);
 }
